cafe51e1b86ef48f02be50e61e452183db9a2e2f
Isa Fontana <isfon8@gmail.com>
2022-02-24 14:04:12 -0700

    new code

--- segmentedControler/ViewController.swift
+++ segmentedControler/ViewController.swift
@@ -7,16 +7,70 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
-//Label: Hello
+    
+    
+    
+
+//Label: Hello Languages
     @IBOutlet weak var languageLabel: UILabel!
     
     @IBOutlet weak var segmentControlOutlet: UISegmentedControl!
     
+    
+    
+//Label: view picker
+    @IBOutlet weak var viewPickerLabel: UILabel!
+    
+    @IBOutlet weak var picker: UIPickerView!
+    
+    let colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Purple"]
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+        switch component {
+        case 0:
+            viewPickerLabel.text = "Red"
+        case 1:
+            viewPickerLabel.text = "Orange"
+        case 2:
+            viewPickerLabel.text = "Yellow"
+        case 3:
+            viewPickerLabel.text = "Green"
+        case 4:
+            viewPickerLabel.text = "Blue"
+        case 5:
+            viewPickerLabel.text = "Indigo"
+        case 6:
+            viewPickerLabel.text = "Purple"
+            
+        default: break
+            
+        }
+        return colors.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return colors[row]
+
+    }
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        picker.dataSource = self
+        picker.delegate = self
+        
+        
+       let titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.gray]
+        segmentControlOutlet.setTitleTextAttributes(titleTextAttributes, for: .normal)
+    
     }
     @IBAction func indexChanged(_ sender: UISegmentedControl) {
         

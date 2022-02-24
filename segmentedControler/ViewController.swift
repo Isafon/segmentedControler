fdd29d13c1457e3be733efaeb8a591e66f6d9b8f
Isa Fontana <isfon8@gmail.com>
2022-02-24 13:14:17 -0700

    Finished Product before additions

--- segmentedControler/ViewController.swift
+++ segmentedControler/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+//Label: Hello
+    @IBOutlet weak var languageLabel: UILabel!
+    
+    @IBOutlet weak var segmentControlOutlet: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func indexChanged(_ sender: UISegmentedControl) {
+        
+        switch segmentControlOutlet.selectedSegmentIndex
+        {
+        case 0:
+                languageLabel.text = "Hello"
+        case 1:
+                languageLabel.text = "你好"
+            
+        case 2: languageLabel.text = "안녕하세요"
+            
+        case 3: languageLabel.text = "Hola"
+            
+            default:
+                break
+            }
+        
+        
+    }
+    
 
 }
 
